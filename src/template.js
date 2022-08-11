e33e7c692c91d7b40a6e04277e74a43c215d5c55
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-08-11 08:31:35 -0700

    adds ReadMe.md structure

---
+++ src/template.js
@@ -0,0 +1,29 @@
+function makeTemplate(data) {
+    return `# ${data.title}
+    ## Licensing:
+    [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
+    ## Table of Contents 
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contribution](#contribution)
+    - [Testing](#testing)
+    - [Additional Info](#additional-info)
+    ## Description:
+    ${data.description}
+    ## Installation:
+    ${data.installation}
+    ## Usage:
+    ${data.usage}
+    ## License:
+    ${data.licensing}
+    ## Contribution:
+    ${data.contribution}
+    ## Testing:
+    ${data.testing}
+    ## Additional Info:
+    - Github: [${data.github}](https://github.com/${data.github})
+    - Email: ${data.email} `;
+  }
+  
+  module.exports = makeTemplate;
