ea8f74391431750ee0d3dfd343f81b5324923fbe
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-08-11 09:03:27 -0700

    adds questions in index.js to correspond to template

--- index.js
+++ index.js
@@ -1,3 +1,125 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const template = require('./src/template')
+const template = require('./src/template');
+
+const questions = [
+    // Licensing
+    {
+        type: 'checkbox',
+        name: 'licensing',
+        message: 'Choose a license for your project (Required)',
+        choices: ['Apache', 'Boost','Common-Development-and Distribution', 'GNU-General-Public', 'MIT', 'Mozilla-Public',  'None'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                console.log('You must pick a license for the project!');
+                return false;
+            }
+        }
+    },
+    // Project name
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your application? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a title to continue!');
+                return false;
+            }
+        }
+    },
+    // Description of application
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the application (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to provide a application description to continue!');
+                return false;
+            }
+        }
+    },
+    // Installation Instructions
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your application? (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('You need to provide installation info to continue!');
+                return false;
+            }
+        }
+    },
+    // Usage Information
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this application? (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to use application to continue!');
+                return false;
+            }
+        }
+    },
+    // Contribution Guidlines
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How should people contribute to this application? (Required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to contribute to the application!');
+                return false;
+            }
+        }
+    },
+    // Test Instructions 
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How do you test this application? (Required)',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('You need to describe how to test this application to continue!');
+                return false;
+            }
+        }
+    },
+    // Github Username
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Missing your GitHub username!');
+                return false;
+            }
+        }
+    },
+    // Email Address
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please insert your email!',
+    },
+];
